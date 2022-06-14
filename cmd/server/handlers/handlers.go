89c642c6b4d425bbcf631f6756c72def83fae9ad
Nechitaylo Vladimir <itbuslave@gmail.com>
2022-06-15 01:03:26 +0300

    [incr-4]: Add empty json response

--- cmd/server/handlers/handlers.go
+++ cmd/server/handlers/handlers.go
@@ -213,5 +213,5 @@ func MetricHandleFromBody(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(""))
+	w.Write([]byte("{}"))
 }
