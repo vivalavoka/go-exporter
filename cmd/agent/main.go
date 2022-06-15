fdb02f59c2ac20020c8ff19c01e8c45ecd2e56ca
Nechitaylo Vladimir <itbuslave@gmail.com>
2022-06-15 23:56:49 +0300

    [incr-5]: Fix 8080 agent port

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 type Config struct {
-	Address        string `env:"ADDRESS" envDefault:"127.0.0.1"`
+	Address        string `env:"ADDRESS" envDefault:"127.0.0.1:8080"`
 	ReportInterval int    `env:"REPORT_INTERVAL" envDefault:"10"`
 	PollInterval   int    `env:"POLL_INTERVAL" envDefault:"2"`
 }
